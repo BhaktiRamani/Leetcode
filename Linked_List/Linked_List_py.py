53cefbddad31d16802fab3a82321751f314e9155
BhaktiRamani <bhakti.ramani2020@gmail.com>
2025-03-10 11:43:20 -0600

    Linked List in Python

---
+++ Linked_List/Linked_List_py.py
@@ -0,0 +1,134 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self._next = None
+       
+        
+class LinkedList():
+    def __init__(self):
+        self.head = None
+        
+    def add(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        else:
+            temp = self.head
+            while temp._next is not None:
+                temp = temp._next
+            temp._next = new_node
+            
+    def insert_at_begin(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        else:
+            new_node.next = self.head
+            self.head = new_node
+            
+    def insert_at_index(self, data, index):
+        if (index == 0):
+            self.insert_at_begin(data)
+            return
+        
+        position = 0
+        current_node = self.head
+        while position != (index - 2):
+            current_node = current_node._next
+            position+=1
+
+  
+        if current_node != None:
+            print(" ")
+            new_node = Node(data)
+            new_node._next = current_node._next
+            current_node._next = new_node
+        else:
+            print("index not present")
+        
+    def insert_at_end(self, data):
+        temp = self.head
+        while temp ._next != None:
+            temp = temp._next
+            
+        new_node = Node(data)
+        temp._next = new_node
+        
+    def update_node(self, data, index):
+        if index == 0 and self.head != None:
+            self.head.data = data
+            return
+        
+        position = 0
+        current_node = self.head
+        while position != index - 1 and current_node != None:
+            current_node = current_node._next
+            position +=1
+         
+        if current_node != None: 
+            current_node.data = data
+        else:
+            print("index for update not present")
+            
+    def delete_node(self, index):
+        if self.head is None:
+            print("Linked List doesn't exist")
+            return
+        if(index == 1):
+            temp = self.head._next
+            self.head = temp
+            return
+        
+        if(index == 0xFF):
+            current_node = self.head
+            while current_node._next._next != None:
+                current_node = current_node._next
+                
+            current_node._next = None
+        
+        position = 0
+        current_node = self.head
+        while position != index - 2 and current_node != None:
+            current_node = current_node._next
+            position += 1
+            
+
+        if current_node != None: 
+            temp = current_node._next._next
+            current_node._next = temp
+        else:
+            print("index for delete not present")
+            
+    def delete_head(self):
+        self.delete_node(1)
+        
+    def delete_end(self):
+        self.delete_node(0xFF)
+        
+
+        
+         
+    def print_ll(self):
+        temp = self.head
+        while temp != None:
+            print(temp.data)
+            temp = temp._next
+            
+            
+            
+ll = LinkedList()
+ll.add(1)
+ll.add(2)
+ll.add(3)
+ll.add(4)
+ll.add(5)
+ll.insert_at_index(6, 3)
+ll.insert_at_end(7)
+ll.update_node(8,10)
+ll.delete_head()
+ll.delete_end()
+ll.print_ll()
+
+            
---
+++ Linked_List/doubly_linked_list_py.py
@@ -0,0 +1,76 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self._prev = None
+        self._next = None
+        
+class doubly_linked_list():
+    def __init__(self):
+        self.head = None
+        
+    def insert_at_end(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        
+        temp = self.head
+        while temp._next != None:
+            temp = temp._next
+            
+        temp._next = new_node
+        new_node._prev = temp
+        
+    def insert_at_begin(self, data):
+        new_node = Node(data)
+        if self.head != None:
+            new_node._next = self.head
+            self.head = new_node
+            return
+        self.head = new_node
+        
+        
+    def insert_at_index(self, data, index):
+        if index == 1:
+            self.insert_at_begin(data)
+            return
+
+        temp = self.head
+        position = 0
+        
+        while position != index - 2 and temp != None:
+            position+=1
+            temp = temp._next
+            print(f"Position : {position} Temp.data : {temp.data} Temp.next.data : {temp._next.data}")
+            
+            
+        print(f"Temp._next.data : {temp._next.data} temp._prev.data {temp._prev.data}")
+        new_node = Node(data)
+        new_node._next = temp._next
+        new_node._prev = temp
+        if temp._next is not None:
+            temp._next._prev = new_node
+        temp._next = new_node
+        print(f"new_node.data : {new_node.data} new_node._next.data : {new_node._next.data} new_node._prev.data {new_node._prev.data}")
+        
+        
+    def print_ll(self):
+        temp = self.head
+        while temp != None:
+            print(temp.data)
+            temp = temp._next
+            
+            
+d_ll = doubly_linked_list()
+
+d_ll.insert_at_begin(1)
+d_ll.insert_at_end(2)
+d_ll.insert_at_end(3)
+d_ll.insert_at_end(4)
+d_ll.insert_at_end(5)
+
+d_ll.insert_at_index(6, 3)
+
+d_ll.print_ll()
+            
+        
